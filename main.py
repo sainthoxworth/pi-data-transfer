97dacb4430dfed51f8adbded7d05472e72d6acf1
Ahmet Faruk Sırma <ahmetfaruk2000@windowslive.com>
2022-06-01 15:11:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,6 @@ try:
     while True:
         received_data = (str)(ser.readline())                   #read NMEA string received
         GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string
-        print (received_data)                                   #DENEME
         if (GPGGA_data_available>0):
             GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
             NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
